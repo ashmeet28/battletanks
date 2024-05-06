0be74027a5372bd4899813115acbecbce4b9f277
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-05-06 18:09:59 -0400

    Added input gadring code

--- Tank.gd
+++ Tank.gd
@@ -41,7 +41,7 @@ func neural_network_mutate(neural_network, bias_mut_val, weight_mut_val):
 		neural_network_layer_mutate(neural_network["layers"][i], bias_mut_val, weight_mut_val)
 
 
-@export var is_ai_controlled:bool = false
+var is_ai_controlled:bool = false
 
 const MAX_SPEED = 500.0
 
@@ -53,34 +53,88 @@ var tank_id:int
 var tank_life:int = 100
 var tank_kills:int = 0
 
+var tank_neural_network
+
 func _ready():
 	tank_id = $"/root/PlaygroundGlobalVariables".next_tank_id
 	$"/root/PlaygroundGlobalVariables".next_tank_id += 1
 	missile_last_fired = Engine.get_physics_frames() - (MISSILE_COOLDOWN*2)
 	if is_ai_controlled:
 		$Camera2D.enabled = false
-	add_to_group("tanks")
 
 func _physics_process(delta):
+	var nn_output = []
+	if !is_ai_controlled && (Engine.get_physics_frames() % 30) == 0:
+		var nn_input = []
+		var single_input = 0.0
+		
+		single_input = 0.0
+		if $RayCast2D.is_colliding() && $RayCast2D.get_collider().is_in_group("tanks"):
+			single_input = 1.0
+		nn_input.append(single_input)
+		
+		single_input = 10000.0
+		if $RayCast2D.is_colliding() && $RayCast2D.get_collider().is_in_group("tanks"):
+			single_input = position.distance_to($RayCast2D.get_collision_point())
+		nn_input.append(single_input)
+		
+		single_input = 0.0
+		if $RayCast2D.is_colliding() && $RayCast2D.get_collider().is_in_group("walls")	:
+			single_input = 1.0
+		nn_input.append(single_input)
+		
+		single_input = 10000.0
+		if $RayCast2D.is_colliding() && $RayCast2D.get_collider().is_in_group("walls"):
+			single_input = position.distance_to($RayCast2D.get_collision_point())
+		nn_input.append(single_input)
+		
+		single_input = 0.0
+		for b in $FrontArea2D.get_overlapping_bodies():
+			if b.is_in_group("walls"):
+				single_input = 1.0
+		nn_input.append(single_input)
+
+		single_input = 0.0
+		for b in $BackArea2D.get_overlapping_bodies():
+			if b.is_in_group("walls"):
+				single_input = 1.0
+		nn_input.append(single_input)
+
+		var area_sensors_for_tanks = [$Area2D2,$Area2D3,$Area2D4,
+		$Area2D5,$Area2D6,$Area2D7,
+		$Area2D8,$Area2D9,$Area2D10,
+		$Area2D11,$Area2D12,$Area2D13,
+		$Area2D14,$Area2D15,$Area2D16,
+		$Area2D17,$Area2D18,$Area2D19,
+		$Area2D22]
+		
+		for a in area_sensors_for_tanks:
+			single_input = 0.0
+			for b in a.get_overlapping_bodies():
+				if b.is_in_group("tanks"):
+					single_input = 1.0
+			nn_input.append(single_input)
+		print(nn_input)
+		#neural_network_process(tank_neural_network, [])
+
 	velocity = Vector2.ZERO
-	if !is_ai_controlled:
-		if Input.is_action_pressed("tank_move_forward"):
-			velocity.y = -MAX_SPEED
-		if Input.is_action_pressed("tank_move_backwards"):
-			velocity.y = MAX_SPEED
+	if Input.is_action_pressed("tank_move_forward") && !is_ai_controlled:
+		velocity.y = -MAX_SPEED
+	if Input.is_action_pressed("tank_move_backwards") && !is_ai_controlled:
+		velocity.y = MAX_SPEED
+	
+	if Input.is_action_pressed("tank_turn_right") && !is_ai_controlled:
+		rotate(PI * delta)
+	if Input.is_action_pressed("tank_turn_left") && !is_ai_controlled:
+		rotate(-(PI * delta))
 		
-		if Input.is_action_pressed("tank_turn_right"):
-			rotate(PI * delta)
-		if Input.is_action_pressed("tank_turn_left"):
-			rotate(-(PI * delta))
-		
-		velocity = velocity.rotated(rotation)
+	velocity = velocity.rotated(rotation)
 	
 	move_and_slide()
 
 	if Input.is_action_pressed("tank_fire") && !is_ai_controlled:
 		var curr_frame = Engine.get_physics_frames()
-		if (curr_frame > MISSILE_COOLDOWN + missile_last_fired) && !($Area2D.has_overlapping_bodies()):
+		if (curr_frame > MISSILE_COOLDOWN + missile_last_fired) && !($FrontArea2D.has_overlapping_bodies()):
 			missile_last_fired = curr_frame
 			var instance = preload("res://tank_missile.tscn").instantiate()
 			instance.position = position + Vector2(0, -160).rotated(rotation)
@@ -89,5 +143,4 @@ func _physics_process(delta):
 			get_parent().add_child(instance)
 
 	if tank_life <= 0:
-		remove_from_group("tanks")
 		queue_free()
--- playground.gd
+++ playground.gd
@@ -1,1 +1,9 @@
 extends Node2D
+
+func _ready():
+	var instance = preload("res://tank.tscn").instantiate()
+	add_child(instance)
+	var instance2 = preload("res://tank.tscn").instantiate()
+	instance2.is_ai_controlled = true
+	instance2.position.x += 250
+	add_child(instance2)
