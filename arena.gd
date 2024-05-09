75691776646b1bd922bb1e0027ae7326e57355e2
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-05-09 16:55:43 -0400

    Starting reset of astar

--- arena.gd
+++ arena.gd
@@ -5,33 +5,18 @@ func get_next_tank_id():
 	current_tank_id += 1
 	return current_tank_id
 
-var ptank
-var btank
-
 func _ready():
 	var instance = preload("res://tank.tscn").instantiate()
 	instance.position = Vector2(0, 500)
 	instance.tank_id = get_next_tank_id()
-	ptank = instance
 	add_child(instance)
 	
 	var instance2 = preload("res://bot_tank.tscn").instantiate()
-	instance2.position = Vector2(600, 1100)
+	instance2.position = Vector2(600, 700)
 	instance2.tank_id = get_next_tank_id()
-	btank = instance2
+	instance2.target_tank_id = instance.tank_id
 	add_child(instance2)
 
 func _process(_delta):
-	var p1 = ArenaGlobalVariables.bot_tank_astar.get_closest_point(btank.position)
-	var p2 = ArenaGlobalVariables.bot_tank_astar.get_closest_point(ptank.position)
-	var p_list = ArenaGlobalVariables.bot_tank_astar.get_id_path(p1, p2)
-	for p in get_tree().get_nodes_in_group("point_validators"):
-		p.queue_free()
-	
-	for i in p_list:
-		var instance = preload("res://tank_point_validator.tscn").instantiate()
-		instance.position = ArenaGlobalVariables.bot_tank_astar.get_point_position(i)
-		instance.validator_id = i
-		add_child(instance)
-
+	pass
 
--- bot_tank.gd
+++ bot_tank.gd
@@ -9,9 +9,46 @@ var tank_id:int
 var tank_life:int = 100
 var tank_damage_given:int = 0
 
+var target_tank_id:int
+
 func _ready():
 	missile_last_fired = Time.get_ticks_msec()
+	rotation = randf_range(0, 2*PI)
 
-func _physics_process(_delta):
+func _physics_process(delta):
+	var target_tank_position
+	for t in get_tree().get_nodes_in_group("tanks"):
+		if t.tank_id == target_tank_id:
+			target_tank_position = t.position
+	var p1 = position
+	var p2 = target_tank_position
+
+	var p1id = ArenaGlobalVariables.bot_tank_astar.get_available_point_id()
+	ArenaGlobalVariables.bot_tank_astar.add_point(p1id, Vector2(p1.x, p1.y))
+	ArenaGlobalVariables.bot_tank_astar.connect_points(
+		p1id,
+		ArenaGlobalVariables.bot_tank_astar.get_closest_point(Vector2(p1.x, p1.y)), false)
+	
+	var p2id = ArenaGlobalVariables.bot_tank_astar.get_available_point_id()
+	ArenaGlobalVariables.bot_tank_astar.add_point(p2id, Vector2(p2.x, p2.y))
+	ArenaGlobalVariables.bot_tank_astar.connect_points(
+		ArenaGlobalVariables.bot_tank_astar.get_closest_point(Vector2(p2.x, p2.y)),
+		p2id, false)
+
+	var p_list = ArenaGlobalVariables.bot_tank_astar.get_id_path(p1id, p2id)
+	if p_list.size() > 0:
+		var p_next = ArenaGlobalVariables.bot_tank_astar.get_point_position(p_list[0])
+		var v = position.direction_to(p_next).rotated(rotation)
+		print(v)
+		if v.x < 0:
+			rotate(-(PI * delta))
+		if v.x > 0:
+			rotate((PI * delta))
+	ArenaGlobalVariables.bot_tank_astar.remove_point(p1id)
+	ArenaGlobalVariables.bot_tank_astar.remove_point(p2id)
+	velocity = Vector2.ZERO
+	velocity.y = -MAX_SPEED
+	velocity = velocity.rotated(rotation)
+	move_and_slide()
 	if tank_life <= 0:
 		queue_free()
