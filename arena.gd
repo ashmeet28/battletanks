95652a6c9d490eb3a74247219b94ea6b7feca9fc
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-05-09 02:17:30 -0400

    Improved path finder

--- arena.gd
+++ arena.gd
@@ -7,9 +7,10 @@ func get_next_tank_id():
 
 #func tank_path_points_spaw():
 	#var tank_path_points = []
-	#for y in range(26):
-		#for x in range(26):
-			#tank_path_points.append([[(x*160)-2000,(y*160)-2000],1])
+	#var sl = 250
+	#for y in range(17):
+		#for x in range(17):
+			#tank_path_points.append([[(x*sl)-2000,(y*sl)-2000],1])
 	#for i in range(tank_path_points.size()):
 		#var instance = preload("res://tank_point_validator.tscn").instantiate()
 		#instance.position = Vector2(tank_path_points[i][0][0], tank_path_points[i][0][1])
@@ -26,6 +27,13 @@ func get_next_tank_id():
 					#p.queue_free()
 					#break
 
+#func tank_path_points_get_invalid_point_indexes(p):
+	#var invalid_p = []
+		#for i in range(p.size()):
+			#if p[i][1] == 0:
+				#invalid_p.append(i)
+	#return invalid_p
+	
 var ptank
 var btank
 
@@ -41,7 +49,6 @@ func _ready():
 	instance2.tank_id = get_next_tank_id()
 	btank = instance2
 	add_child(instance2)
-	
 
 func _process(_delta):
 	var astar = ArenaGlobalVariables.bot_tank_astar
@@ -53,6 +60,6 @@ func _process(_delta):
 	
 	for i in p_list:
 		var instance = preload("res://tank_point_validator.tscn").instantiate()
-		var p = ArenaGlobalVariables.bot_tank_valid_path_points[i]
-		instance.position = Vector2(p[0], p[1])
+		instance.position = astar.get_point_position(i)
 		add_child(instance)
+
